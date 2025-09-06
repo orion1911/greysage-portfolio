4f344bfee385c11efb00f0745a079dc0a9f4849c
techdev19 <adilhk1911@gmail.com>
2025-09-06 21:39:16 +0400

    delay loading

--- src/root.jsx
+++ src/root.jsx
@@ -1,18 +1,16 @@
-import React, { use, useEffect, useState } from 'react';
+import React, { lazy, use, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate, Outlet } from 'react-router-dom';
 import { ThemeProvider, themeStyles } from './components/theme-provider';
 import { Navbar } from './components/navbar/navbar';
 import styles from './root.module.css';
 import './reset.module.css';
 import './global.module.css';
-import Home from './features/home';
-// import Sphere3D from './features/sphere3D';
+const Home = lazy(() => import('./features/home'));
 
 const Root = () => {
   const { state } = useNavigate();
   return (
     <>
-    {/* <Sphere3D /> */}
     <Navbar />
     <main
             id="main-content"
@@ -27,43 +25,28 @@ const Root = () => {
 }
 
 function App() {
-  // LicenseInfo.setLicenseKey(process.env.REACT_APP_MUI_LICENSE_KEY);
-
-  // const [variant, setVariant] = React.useState('purple');
-  // const [darkMode, setDarkMode] = React.useState(false);
-
-  // const theme = useTheme();
-  // const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
+  const [isReady, setIsReady] = useState(false);
   const [theme, setTheme] = useState('light');
-  // const theme = session.get('theme') || 'dark';
-  // const theme = 'light';
 
   const toggleTheme = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark';
     setTheme(newTheme);
-  }
+  };
 
   useEffect(() => {
     const styleElement = document.createElement('style');
     styleElement.innerHTML = themeStyles;
     document.head.appendChild(styleElement);
-
-    // const canvasElement = document.createElement('canvas');
-    // canvasElement.id = 'scene';
-    // document.body.appendChild(canvasElement);
-
-    // const script = document.createElement('script');
-    // script.src = './sphere3d.js';
-    // script.async = true;
-    // document.html.appendChild(script);
+    setIsReady(true); // Set ready after styles are applied
 
     return () => {
       document.head.removeChild(styleElement);
-      // document.body.removeChild(canvasElement);
-      // document.html.removeChild(script);
     };
   }, []);
+
+  if (!isReady) {
+    return null; // Or a loading spinner
+  }
 
 
   return (
